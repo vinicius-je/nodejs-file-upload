a46fafe51d67acac45c7c8b22aa7deffedbf14bc
vinicius-je <vinicius.je99@gmail.com>
2022-06-17 22:27:30 -0300

    middlewares added in index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const fileUpload = require('express-fileupload');
 const path = require('path');
 
+// middlewares
+const verifyFilePayload = require('./middleware/verifyFilePayload');
+const fileSizeLimiter = require('./middleware/fileSizeLimiter');
+const fileExtensionLimiter = require('./middleware/fileExtensionLimiter');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -10,7 +15,15 @@ app.get('/', (req, res) => {
 })
 
 app.post('/upload', 
-    fileUpload({ createParentPath: true }))
+    fileUpload({ createParentPath: true }),
+    verifyFilePayload,
+    fileExtensionLimiter(['.png', '.jpg', '.jpeg']),
+    fileSizeLimiter,
+    (req, res) => {
+        const files = req.files;
+        console.log(files);
+    }
+)
 
 app.listen(3000, () => {
     console.log('Server Running on PORT: 3000');
