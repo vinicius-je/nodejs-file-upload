861a8f02f6636857b897bd2cbfa23c40f458c7d4
vinicius-je <vinicius.je99@gmail.com>
2022-06-17 21:57:25 -0300

    verify file payload middleware

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
 const path = require('path');
-const dileUpload = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
+
+app.post('/upload', 
+    fileUpload({ createParentPath: true }))
 
 app.listen(3000, () => {
     console.log('Server Running on PORT: 3000');
